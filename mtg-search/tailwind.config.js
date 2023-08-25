5311a0e3066b14ec958ea0e7971ca451fd5d9a33
ScottKenning1 <skenning128@gmail.com>
2023-08-25 00:53:58 -0700

    fix webpack

--- mtg-search/tailwind.config.js
+++ mtg-search/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{html,ts}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
